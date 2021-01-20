da84051a74348ed861e0c004796d0fef26d9fb93
Juan Miguel Garcia Sierra <juan.gmsierra@gmail.com>
2021-01-19 22:17:45 -0600

    config carousel

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,33 @@
 import Header from "../layout/header";
+import "react-multi-carousel/lib/styles.css";
+import Section from "../components/Section";
+import Simple from "../components/Simple";
+import React, { Fragment } from "react";
+import UAParser from "ua-parser-js";
 
-export default function AboutPage() {
-  return <div>
-    <Header />
-    About us</div>
+const AboutPage = ({ deviceType }) => {
+  return (
+    <Fragment>
+      <Header />
+      <Section>
+        <Simple deviceType={deviceType} />
+      </Section>
+      
+    </Fragment>
+    )
 }
+
+AboutPage.getInitialProps = ({ req }) => {
+  let userAgent;
+  if (req) {
+    userAgent = req.headers["user-agent"];
+  } else {
+    userAgent = navigator.userAgent;
+  }
+  const parser = new UAParser();
+  parser.setUA(userAgent);
+  const result = parser.getResult();
+  const deviceType = (result.device && result.device.type) || "desktop";
+  return { deviceType };
+};
+export default AboutPage;
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Typography from "@material-ui/core/Typography";
 import Header from "../layout/header";
 
 export default function IndexPage() {
+
   return (
     <div>
       <Header />
