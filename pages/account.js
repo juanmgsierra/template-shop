a9faf47637e68fc1c1cfbe1c5e05af85489d4467
Juan Miguel Garcia Sierra <juan.gmsierra@gmail.com>
2021-03-01 21:53:11 -0600

    account page settings

--- pages/account.js
+++ pages/account.js
@@ -1,11 +1,56 @@
 import Header from "../layout/header";
+import { makeStyles } from '@material-ui/core/styles';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import TabPanel from '../components/TabPanel'
 
-export default function account() {
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+        backgroundColor: theme.palette.background.paper,
+        display: 'flex',
+        height: 524,
+    },
+    tabs: {
+        borderRight: `1px solid ${theme.palette.divider}`,
+    },
+}));
+
+export default function Account() {
+    const classes = useStyles();
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
 
     return (
-        <div>
+        <>
             <Header />
-            <h1>Account</h1>
-        </div>
-    )
+            <div className={classes.root}>
+                <Tabs
+                    orientation="vertical"
+                    variant="scrollable"
+                    value={value}
+                    onChange={handleChange}
+                    aria-label="Vertical tabs example"
+                    className={classes.tabs}
+                >
+                    <Tab label="Cuenta"  />
+                    <Tab label="Direcciones"  />
+                    <Tab label="Ordenes"  />                   
+                    <Tab label="Cerrar sesión"  />
+                </Tabs>
+                <TabPanel value={value} index={0}>
+                    Item One
+                </TabPanel>
+                <TabPanel value={value} index={1}>
+                    Item Two
+                </TabPanel>
+                <TabPanel value={value} index={2}>
+                    Item Three
+                </TabPanel>            
+            </div>
+        </>
+    );
 }
