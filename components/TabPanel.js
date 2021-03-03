e200b90e557241ec9b39f05c517f7c7eb2202bca
Juan Miguel Garcia Sierra <juan.gmsierra@gmail.com>
2021-03-02 22:55:30 -0600

    account page settings v1.0

--- components/TabPanel.js
+++ components/TabPanel.js
@@ -1,29 +1,14 @@
 import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
 import PropTypes from 'prop-types';
 
 export default function TabPanel(props) {
-    const { children, value, index, ...other } = props;
-
     return (
-        <div
-            role="tabpanel"
-            hidden={value !== index}
-            id={`vertical-tabpanel-${index}`}
-            aria-labelledby={`vertical-tab-${index}`}
-            {...other}
-        >
-            {value === index && (
-                <Box p={3}>
-                    <Typography>{children}</Typography>
-                </Box>
-            )}
-        </div>
+        <Typography component="div" style={{ padding: 8 * 3 }}>
+            {props.children}
+        </Typography>
     );
 }
 
 TabPanel.propTypes = {
-    children: PropTypes.node,
-    index: PropTypes.any.isRequired,
-    value: PropTypes.any.isRequired,
+    children: PropTypes.node.isRequired,
 };
--- pages/account.js
+++ pages/account.js
@@ -3,53 +3,137 @@ import { makeStyles } from '@material-ui/core/styles';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import TabPanel from '../components/TabPanel'
+import { useState } from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import { useSelector } from 'react-redux';
 
 const useStyles = makeStyles((theme) => ({
     root: {
         flexGrow: 1,
+        width: '100%',
         backgroundColor: theme.palette.background.paper,
+        height: 600,
         display: 'flex',
-        height: 524,
+        flexDirection: 'column',
+        alignItems: 'center',
     },
     tabs: {
         borderRight: `1px solid ${theme.palette.divider}`,
     },
+    form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing(2),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
 }));
 
+const initialState = {
+    email: "",
+    password: ""
+}
+
 export default function Account() {
-    const classes = useStyles();
-    const [value, setValue] = React.useState(0);
+    const classes = useStyles();    
+    const { user } = useSelector(state => state.session);
+    const [value, setValue] = useState(0);
+    const [usuario, setUsuario] = useState(user)
+
 
     const handleChange = (event, newValue) => {
         setValue(newValue);
+    };
+
+    const onChange = e => {
+        const { name, value } = e.target;
+        setUsuario((prev) => ({
+            ...prev,
+            [name]: value,
+        }));
     };
 
     return (
         <>
             <Header />
             <div className={classes.root}>
-                <Tabs
-                    orientation="vertical"
-                    variant="scrollable"
-                    value={value}
-                    onChange={handleChange}
-                    aria-label="Vertical tabs example"
-                    className={classes.tabs}
-                >
-                    <Tab label="Cuenta"  />
-                    <Tab label="Direcciones"  />
-                    <Tab label="Ordenes"  />                   
-                    <Tab label="Cerrar sesión"  />
-                </Tabs>
-                <TabPanel value={value} index={0}>
-                    Item One
-                </TabPanel>
-                <TabPanel value={value} index={1}>
-                    Item Two
-                </TabPanel>
-                <TabPanel value={value} index={2}>
-                    Item Three
-                </TabPanel>            
+                <AppBar position="static" color="default">
+                    <Tabs
+                        value={value}
+                        onChange={handleChange}
+                        indicatorColor="primary"
+                        textColor="primary"
+                        scrollButtons="auto"
+                        centered
+                        //variant="fullWidth"
+                    >
+                        <Tab label="Cuenta" />
+                        <Tab label="Direcciones" />
+                        <Tab label="Ordenes" />                        
+                    </Tabs>
+                </AppBar>
+                {value === 0 && <TabPanel>
+                    <form className={classes.form} noValidate>
+                        <Grid container spacing={1}>
+                            <TextField
+                                variant="outlined"
+                                margin="normal"
+                                required
+                                fullWidth
+                                label="Nombre completo"
+                                name="email"
+                                value={usuario.nombre}
+                                onChange={onChange}
+                                autoComplete="email"
+                                autoFocus
+                            />
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                variant="outlined"
+                                margin="normal"
+                                required
+                                fullWidth
+                                label="Correo"
+                                name="email"
+                                value={usuario.email}
+                                disabled
+                                onChange={onChange}
+                                autoComplete="email"                                
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                variant="outlined"
+                                margin="normal"
+                                required
+                                fullWidth
+                                name="password"
+                                label="Celular"
+                                type="password"
+                                value={usuario.celular}
+                                autoComplete="current-password"
+                                onChange={onChange}
+                            />
+                        </Grid>
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                color="primary"
+                                //onClick={login}
+                                className={classes.submit}
+                            >
+                                Guardar
+                        </Button>
+                        </Grid>
+                    </form>
+                </TabPanel>}
+                {value === 1 && <TabPanel>Item Two</TabPanel>}
+                {value === 2 && <TabPanel>Item Three</TabPanel>}
+
             </div>
         </>
     );
