4723d2c71a5e507ca2a3df919568ce1af7705a7e
Juan Miguel Garcia Sierra <juan.gmsierra@gmail.com>
2021-03-08 21:39:20 -0600

    account order address

--- pages/account.js
+++ pages/account.js
@@ -1,35 +1,21 @@
 import { useState } from 'react';
 import Header from "../layout/header";
-import { makeStyles } from '@material-ui/core/styles';
-import Tab from '@material-ui/core/Tab';
-import TabContext from '@material-ui/lab/TabContext';
-import TabList from '@material-ui/lab/TabList';
-import TabPanel from '@material-ui/lab/TabPanel'
-import AppBar from '@material-ui/core/AppBar';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
-import Grid from '@material-ui/core/Grid';
+import { TabContext, TabList, TabPanel } from '@material-ui/lab';
+import { AppBar, TextField, Button, Grid, Card, CardActions, CardContent, makeStyles, Typography, Tab } from '@material-ui/core';
 import { useSelector, useDispatch } from 'react-redux';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Typography from '@material-ui/core/Typography';
 import { SESSION_REQUEST } from '../src/constants/actions-types'
 
 
 const useStyles = makeStyles((theme) => ({
     root: {
-        //flexGrow: 1,
         width: '100%',
         backgroundColor: theme.palette.background.paper,
-        //height: 600,
-        display: 'flex',
         flexDirection: 'column',
         alignItems: 'center',
     },
     tabs: {
         borderRight: `1px solid ${theme.palette.divider}`,
-    },    
+    },
     form: {
         width: '100%', // Fix IE 11 issue.
         marginTop: theme.spacing(2),
@@ -38,13 +24,13 @@ const useStyles = makeStyles((theme) => ({
         margin: theme.spacing(3, 0, 2),
     },
     card: {
-        minWidth: 200,
-        display: 'flex',
+        //Width: 20,
+        //display: 'flex',
     },
     gridContainer: {
         paddingLeft: "40px",
         paddingRight: "40px"
-      }
+    }
 }));
 
 export default function Account() {
@@ -54,7 +40,7 @@ export default function Account() {
     const [value, setValue] = useState("1");
     const [usuario, setUsuario] = useState(user)
 
-    const array = [1, 2, 3];
+    const array = [1, 2, 3,4,5];
     const handleChange = (event, newValue) => {
         setValue(newValue);
     };
@@ -87,60 +73,62 @@ export default function Account() {
                             <Tab label="Direcciones" value="2" />
                             <Tab label="Ordenes" value="3" />
                         </TabList>
-                    </AppBar>                   
-                    <TabPanel value="1">                         
-                        <form className={classes.form} noValidate>
-                            <Grid container spacing={1}>
-                                <TextField
-                                    variant="outlined"
-                                    margin="normal"
-                                    required
-                                    fullWidth
-                                    label="Nombre completo"
-                                    name="nombre"
-                                    value={usuario.nombre || ''}
-                                    onChange={onChange}
-                                    autoComplete="nombre"
-                                    autoFocus
-                                />
-                                <Grid item xs={12} sm={6}>
+                    </AppBar>
+                    <TabPanel value="1">
+                        <div style={{ margin: 'auto', maxWidth: 600 }}>
+                            <form className={classes.form} noValidate>
+                                <Grid container spacing={1}>
                                     <TextField
                                         variant="outlined"
                                         margin="normal"
                                         required
                                         fullWidth
-                                        label="Correo"
-                                        name="email"
-                                        value={usuario.email}
-                                        disabled
+                                        label="Nombre completo"
+                                        name="nombre"
+                                        value={usuario.nombre || ''}
                                         onChange={onChange}
-                                        autoComplete="email"
+                                        autoComplete="nombre"
+                                        autoFocus
                                     />
-                                </Grid>
-                                <Grid item xs={12} sm={6}>
-                                    <TextField
-                                        variant="outlined"
-                                        margin="normal"
-                                        required
+                                    <Grid item xs={12} sm={6}>
+                                        <TextField
+                                            variant="outlined"
+                                            margin="normal"
+                                            required
+                                            fullWidth
+                                            label="Correo"
+                                            name="email"
+                                            value={usuario.email}
+                                            disabled
+                                            onChange={onChange}
+                                            autoComplete="email"
+                                        />
+                                    </Grid>
+                                    <Grid item xs={12} sm={6}>
+                                        <TextField
+                                            variant="outlined"
+                                            margin="normal"
+                                            required
+                                            fullWidth
+                                            name="celular"
+                                            label="Celular"
+                                            value={usuario.celular || ''}
+                                            onChange={onChange}
+                                        />
+                                    </Grid>
+                                    <Button
+                                        type="submit"
                                         fullWidth
-                                        name="celular"
-                                        label="Celular"
-                                        value={usuario.celular || ''}
-                                        onChange={onChange}
-                                    />
-                                </Grid>
-                                <Button
-                                    type="submit"
-                                    fullWidth
-                                    variant="contained"
-                                    color="primary"
-                                    onClick={updateProfile}
-                                    className={classes.submit}
-                                >
-                                    Guardar
+                                        variant="contained"
+                                        color="primary"
+                                        onClick={updateProfile}
+                                        className={classes.submit}
+                                    >
+                                        Guardar
                                 </Button>
-                            </Grid>
-                        </form>                  
+                                </Grid>
+                            </form>
+                        </div>
                     </TabPanel>
                     <TabPanel value="2">
                         <Grid
@@ -149,52 +137,31 @@ export default function Account() {
                             className={classes.gridContainer}
                             justify="center"
                         >
-                            <Grid item xs={12} sm={6} md={4}>
-                                <Card className={classes.card} >
-                                    <CardContent>
+                            {array.map(id => (
+                                <Grid item xs={12} sm={6} md={4} key={id} >
+                                    <Card className={classes.card} >
+                                        <CardContent>
                                         <Typography className={classes.title} color="textSecondary" gutterBottom>
                                             Word of the Day
-                                    </Typography>
-                                        <Typography variant="h5" component="h2">
-                                            asdd
-                                    </Typography>
+                                        </Typography>
+                                            <Typography variant="h5" component="h2">
+                                                asdd
+                                        </Typography>
                                         <Typography className={classes.pos} color="textSecondary">
                                             adjective
-                                    </Typography>
-                                        <Typography variant="body2" component="p">
-                                            well meaning and kindly.
-                                    <br />
-                                            {'"a benevolent smile"'}
                                         </Typography>
-                                    </CardContent>
-                                    <CardActions>
-                                        <Button size="small">Learn More</Button>
-                                    </CardActions>
-                                </Card>
-                            </Grid>
-                            <Grid item xs={12} sm={6} md={4}>
-                                <Card className={classes.card} >
-                                    <CardContent>
-                                        <Typography className={classes.title} color="textSecondary" gutterBottom>
-                                            Word of the Day
-                                    </Typography>
-                                        <Typography variant="h5" component="h2">
-                                            asdd
-                                    </Typography>
-                                        <Typography className={classes.pos} color="textSecondary">
-                                            adjective
-                                    </Typography>
-                                        <Typography variant="body2" component="p">
-                                            well meaning and kindly.
-                                    <br />
-                                            {'"a benevolent smile"'}
-                                        </Typography>
-                                    </CardContent>
-                                    <CardActions>
-                                        <Button size="small">Learn More</Button>
-                                    </CardActions>
-                                </Card>
-                            </Grid>
+                                            <Typography variant="body2" component="p">
+                                                well meaning and kindly.
+                                             <br />
+                                                {'"a benevolent smile"'}
+                                            </Typography>
+                                        </CardContent>
+                                        <CardActions>
+                                            <Button size="small">Learn More</Button>
+                                        </CardActions>
+                                    </Card>
+                                </Grid>                                
+                            ) )}
                         </Grid>
                     </TabPanel>
                     <TabPanel value="3">
