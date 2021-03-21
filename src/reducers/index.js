b5f126452f02ef145e88d7e2d1361a532460169a
Juan Miguel Garcia Sierra <juan.gmsierra@gmail.com>
2021-03-20 19:59:56 -0600

    redux persist

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,18 @@
 import { combineReducers } from 'redux';
 import sessionReducer from './session';
 import addressReducer from './address';
+import { persistReducer } from 'redux-persist';
+import storage from '../store/storage';
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist:['session']
+}
 
 const rootReducer = combineReducers({
     session: sessionReducer,
     userAddress: addressReducer
 });
 
-export default rootReducer;
+export default persistReducer(persistConfig, rootReducer);
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,18 @@
 import { createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
+import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
 import rootReducer from '../reducers';
 import rootSaga from '../sagas';
 
 const sagaMiddleware = createSagaMiddleware();
-const store = createStore(
+
+export const store = createStore(
     rootReducer,
     applyMiddleware(sagaMiddleware, logger)
 )
 
 sagaMiddleware.run(rootSaga);
+export const persistor = persistStore(store);
 
-export default store;
+export default { store, persistor };
